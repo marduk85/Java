1a7ea9e84e26ca0460d20360547dd9de54a003e5
marduk85 <rozenfeldrv@gmail.com>
2022-12-09 14:03:58 +0300

    simple-calculator debug

--- simple-calculator/src/Main.java
+++ simple-calculator/src/Main.java
@@ -1,5 +1,28 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        double a; //объявляем первую перменную
+        double b; // объявляем вторую переменную
+        char op;  //объявляем перемнную в которую будем записывать знак операции
+        double result; //для записи результата
+        System.out.println("Я простой калькулятор, введи два числа:\n"); //предлагаем пользователю ввести два числа
+        Scanner read= new Scanner (System.in); //создаем сканер для считывания чисел
+        a = read.nextDouble(); //считываем первое число
+        b = read.nextDouble(); //считываем второе число
+        System.out.println("Введи оператор + - * /\n"); //предлагаем ввести оператор
+        op = read. next().charAt(0); //считываем оператор
+        //все еобходимые дынные считали теперь будет использовать switch-case для решения
+        switch (op) {
+            case '-' -> result = a - b;
+            case '+' -> result = a + b;
+            case '*' -> result = a * b;
+            case '/' -> result = a / b;
+        }
+        System.out.println("Результат:\n");
+        System.out.printf(a + op + b + "=" +result);
+
     }
 }
