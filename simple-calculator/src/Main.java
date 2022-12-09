963d9c021604e7bba0b86bbf7c0bbc91404a1a98
marduk85 <rozenfeldrv@gmail.com>
2022-12-09 14:17:00 +0300

    simple-calculator add comments

--- simple-calculator/src/Main.java
+++ simple-calculator/src/Main.java
@@ -15,26 +15,26 @@ public class Main
         System.out.println("Введи оператор + - * /\n"); //предлагаем ввести оператор
         op = read. next().charAt(0); //считываем оператор
         //все еобходимые дынные считали теперь будет использовать switch-case для решения
-        switch (op) {
-            case '-' -> {
-                result = a - b;
-                System.out.println("Результат:\n");
-                System.out.printf(a + op + b + "=" +result);
+        switch (op) { //оператор выбора
+            case '-' -> { //если в качестве знака выбрали минус
+                result = a - b; //вычисляем результат
+                System.out.println("Результат:\n"); //выводим текст
+                System.out.printf(a +" "+ op +" "+ b + "=" +result); //выводим выражение и его результат
             }
-            case '+' -> {
+            case '+' -> { //если в качестве знака выбрали плюс
                 result = a + b;
                 System.out.println("Результат:\n");
-                System.out.printf(a + op + b + "=" +result);
+                System.out.printf(a +" "+ op +" "+ b + "=" +result);
             }
-            case '*' -> {
+            case '*' -> { //если в качестве знака выбрали умножение
                 result = a * b;
                 System.out.println("Результат:\n");
-                System.out.printf(a + op + b + "=" +result);
+                System.out.printf(a +" "+ op +" "+ b + "=" +result);
             }
-            case '/' -> {
+            case '/' -> { //если в качестве знака выбрали деление
                 result = a / b;
                 System.out.println("Результат:\n");
-                System.out.printf(a + op + b + "=" +result);
+                System.out.printf(a +" "+ op +" "+ b + "=" +result);
             }
         }
 
